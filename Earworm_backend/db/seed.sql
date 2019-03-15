fbe64e811bee8864bf2eadea1530ae70d27daa2c
JJ <jackyong@pursuit.org>
2019-03-15 13:31:53 -0400

    sql seed

--- Earworm_backend/db/seed.sql
+++ Earworm_backend/db/seed.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
 
 CREATE TABLE genres (
   id SERIAL PRIMARY KEY NOT NULL,
-  genre_name VARCHAR UNIQUE NOT NULL
+  type VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE songs (
@@ -28,4 +28,19 @@ CREATE TABLE favorites (
   songs_id INT REFERENCES songs(id) ON DELETE CASCADE
 );
 
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  body VARCHAR,
+  users_id INT REFERENCES users(id),
+  songs_id INT REFERENCES songs(id)
+);
 
+\COPY users(username) FROM '../db/users.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO genres(type) VALUES ('children song'),('pop'),('rock'),('folk'),('theme');
+
+\COPY songs (title,img_url,users_id,genres_id) FROM '../songs.csv' DELIMITER ',' CSV HEADER;
+
+\COPY favorites (users_id,songs_id) FROM '../favorites.csv' DELIMITER ',' CSV HEADER;
+
+\COPY comments (body, users_id, songs_id) FROM '../comments.csv' DELIMITER ',' CSV HEADER;
