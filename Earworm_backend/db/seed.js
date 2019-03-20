2147329c355bf43dbfc7ea41f4ae8eec9b76e3c7
Reed Gaines <reed.gaines@gmail.com>
2019-03-20 16:12:59 -0400

    add comments

--- Earworm_backend/db/seed.js
+++ Earworm_backend/db/seed.js
@@ -1,4 +1,6 @@
 const pgp = require("pg-promise")({})
-const db = pgp("postgres://localhost:5432/earworms")
+const db = pgp("postgres://localhost:5432/earworm")
+
+// just changing this to fit my db name, disregard
 
 module.exports = { db }
