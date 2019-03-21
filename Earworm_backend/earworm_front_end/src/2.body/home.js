fa623b3132c13a91e3e6e2c96632bda398f04098
JJ <jackyong@pursuit.org>
2019-03-21 09:42:20 -0400

    CSS Navi

--- Earworm_backend/earworm_front_end/src/2.body/home.js
+++ Earworm_backend/earworm_front_end/src/2.body/home.js
@@ -1,10 +1,14 @@
 import React from "react"
-
+import "../css/home.css"
 
 
 const Home = (props) => {
   return(
+    <div className = "Home" >
+    <div className = "content_container"> 
     <h1> Earworm Report </h1>
+    </div>
+    </div>
   )
 }
 export default Home
